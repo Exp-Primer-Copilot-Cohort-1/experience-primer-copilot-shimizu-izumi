238cea1d9f052bffbb36ff8bcc311a1a7da59440
Shimizu Izumi <ShimizuIzumi@gmx.net>
2023-09-29 18:43:13 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+  var skills = ["HTML", "CSS", "JS", "React"];
+  var member = {
+    name: "John",
+    age: 25,
+    skills: skills,
+  };
+  // Write code here
+  var memberSkills = member.skills;
+  return memberSkills;
+}
